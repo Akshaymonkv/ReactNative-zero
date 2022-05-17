83a28e59cb4359e067f54cda3f6714c88a8ebfa8
Akshaymonkv <70555498+Akshaymonkv@users.noreply.github.com>
2022-05-17 21:53:16 +0530

    Add files via upload
    Example for operators in JS

---
+++ operator.js
@@ -0,0 +1,32 @@
+//Operators in js.
+
+//Basic arithmetic operators
+//1. Addition operator
+        var a = 5
+        var b = 6
+        var c = a+b;
+        console.log(c);
+
+//2. Substraction operator
+        var a = 5
+        var b = 6
+        var c = b-a;
+        console.log(c);
+
+//3. Multiplication operator
+        var a = 5
+        var b = 6
+        var c = b*a;
+        console.log(c);
+
+//. Division operator
+        var a = 5
+        var b = 6
+        var c = b/a;
+        console.log(c);
+
+//Comparision operators in js//
+/*Similar to the arithmetic operators, the coparision operators are '==','===','!=',
+                                                                    '<=','>=','>','<', '!==' etc */
+
+    
