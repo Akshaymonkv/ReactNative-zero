f96859cde8d0e17e1bb01d79485c02753c1788e7
Akshaymonkv <70555498+Akshaymonkv@users.noreply.github.com>
2022-05-18 21:52:55 +0530

    Conditional statements

---
+++ 2_JavaScript_Basics/if_else.js
@@ -0,0 +1,30 @@
+//Basic example of if, if-else and, if-else-if statements to understand the sytax of the same
+
+//1. If statement
+
+var a = 5
+if( a == 10){
+    console.log('not equal')
+};
+
+//2. If else statement
+var a = 2
+if(a == 2){
+    console.log('Equal')
+}
+else{
+    console.log('Not equal')
+}
+
+//3. If-else-if statement
+
+var s = "Apple"
+if(s == ''){
+    console.log('Empty name')
+}
+else if(s == "Apple"){
+    console.log('S is an Apple')
+}
+else{
+    console.log('S is something else')
+}
