09b18739731aedf259da6c670bff01c3052e700f
Akshaymonkv <70555498+Akshaymonkv@users.noreply.github.com>
2022-05-24 19:26:45 +0530

    Loops

---
+++ 2_JavaScript_Basics/fi_loop.js
@@ -0,0 +1,14 @@
+//for loop example
+
+var x = 10
+for(var i=0;i<x;i++){
+    console.log(i)
+}
+
+//for in loop example
+
+const no = {a:1,b:2}
+for(var key in no){
+    num = no[key]
+    console.log(num)
+}
---
+++ 2_JavaScript_Basics/wd_loop.js
@@ -0,0 +1,13 @@
+//while loop example
+var x =5
+while(x<10){
+    console.log(x)
+    x+=1
+}
+
+//do-while loopo example
+do{
+    console.log(x)
+    x = x-1
+}
+while(x>1)
